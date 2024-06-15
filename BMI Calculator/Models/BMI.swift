2a2ba9c7576314f1bd0e5ad65a34b297320948f2
Kamogelo Malatsi <53654964+SaxWale@users.noreply.github.com>
2024-06-15 13:17:44 +0200

    Add files via upload

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by SaxWalè on 2020/01/07.
+//  Copyright © 2020 SaxWalé Productions. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
